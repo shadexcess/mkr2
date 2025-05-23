f1f0a8e43f5184d03f1e36a16c685b6911d4b213
shadexcess <sarewuyyr@gmail.com>
2025-05-23 16:16:12 +0300

    gallery view added

--- gallery/views.py
+++ gallery/views.py
@@ -1,1 +1,6 @@
 from django.shortcuts import render, get_object_or_404
+from .models import Image, Category
+
+def gallery_view(request):
+    categories = Category.objects.all()
+    return render(request, 'gallery.html', {'categories': categories})
