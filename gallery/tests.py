dd2e9b72a13f5fc73841fb462fad6fc8254a6455
shadexcess <sarewuyyr@gmail.com>
2025-05-23 16:17:55 +0300

    test1

---
+++ gallery/tests.py
@@ -0,0 +1,30 @@
+from django.test import TestCase
+from django.urls import reverse
+from .models import Category, Image
+from django.core.files.uploadedfile import SimpleUploadedFile
+from datetime import date
+
+class GalleryViewsTests(TestCase):
+
+    def setUp(self):
+        self.category = Category.objects.create(name='Category 1')
+        image_content = SimpleUploadedFile(
+            name='test_image.jpg',
+            content=b'\x47\x49\x46\x38\x39\x61',
+            content_type='image/gif'
+        )
+
+        self.image = Image.objects.create(
+            title='Зображення ТЕСТ',
+            image=image_content,
+            created_date=date.today(),
+            age_limit=10,
+        )
+        self.image.categories.add(self.category)
+
+    def test_gallery_correct_template(self):
+        url = reverse('main')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'gallery.html')
+        self.assertIn(self.category, response.context['categories'])
