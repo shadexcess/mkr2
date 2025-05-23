7d4e46baf94b8b7d5d9edaea96b6467e35c01a7d
shadexcess <sarewuyyr@gmail.com>
2025-05-23 16:16:57 +0300

    image detail added view

--- gallery/views.py
+++ gallery/views.py
@@ -4,3 +4,7 @@ from .models import Image, Category
 def gallery_view(request):
     categories = Category.objects.all()
     return render(request, 'gallery.html', {'categories': categories})
+
+def image_detail(request, pk):
+    image = get_object_or_404(Image, pk=pk)
+    return render(request, 'image_detail.html', {'image': image})
